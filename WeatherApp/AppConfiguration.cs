1825e205b494fd54d179334d3234e0bcae4855fe
Olivier21 <olivier.lafleur21@gmail.com>
2021-11-08 21:26:00 -0500

    T3 complété

---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    static public class AppConfiguration
+    {
+        static private IConfiguration configuration;
+        static public string GetValue(string key)
+        {
+            return key;
+        }
+        static private void initConfig()
+        {
+            configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddUserSecrets("f4f5746e-d24c-4697-b6cd-935a16b16033").Build();
+        }
+    }
+}
